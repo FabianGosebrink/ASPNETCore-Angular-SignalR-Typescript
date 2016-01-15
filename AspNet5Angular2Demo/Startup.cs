02e621561fad0376de9d8b793154a4cc917c9256
damienbod <damien_bod@hotmail.com>
2016-01-15 12:56:52 +0100

    removing the IHostingEnvironment

--- AspNet5Angular2Demo/Startup.cs
+++ AspNet5Angular2Demo/Startup.cs
@@ -54,7 +54,7 @@ namespace AspNet5Angular2Demo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
