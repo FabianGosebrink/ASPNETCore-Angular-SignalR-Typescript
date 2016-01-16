5ef36699f4720be1c0f8e2637af5f7c9f43b111f
FabianGosebrink <1oSzfcWKTk9raYjytkAT>
2016-01-16 17:34:54 +0100

    small refactorings and deleting all *.js and *.js.map-files because they can be compiled before executing on client side

--- src/AspNet5Angular2Demo/wwwroot/app/food/foodForm.ts
+++ src/AspNet5Angular2Demo/wwwroot/app/food/foodForm.ts
@@ -4,14 +4,13 @@ import { DataService } from '../services/foodDataService';
 
 @Component({
     selector: 'food-form',
-    template: `
-    <form (ngSubmit)="addFood()">
-      <input type="text" [(ngModel)]="foodName" size="30" placeholder="add new food here">
-      <input class="btn-primary" type="submit" value="add">
-    </form>`
+    templateUrl: 'app/food/foodForm.component.html'
 })
 export class FoodForm {
-    @Output() newFoodItem = new EventEmitter<IFoodItem>();
+    
+    //@Output() newFoodItem = new EventEmitter<IFoodItem>();
+    @Output() newFoodItem = new EventEmitter();
+    
     public foodName: string;
 
     constructor(private _dataService: DataService) {
@@ -19,15 +18,12 @@ export class FoodForm {
     }
 
     addFood() {
-        
-        console.log(this.foodName);
-        
         this._dataService
         .AddFood(this.foodName)
         .subscribe(data => {
-                console.log("Added Food" + data);
-                this.newFoodItem.next(data);
-            }, err => console.log(err),
+                this.newFoodItem.emit("event");
+            }, err => {
+                console.log(err)},
             () => console.log('Call Complete'));
     }
 }
