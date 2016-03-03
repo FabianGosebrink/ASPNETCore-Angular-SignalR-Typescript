c91f1b639cea37826aa596ef5f7239a5efc66b15
FabianGosebrink <fabian.gosebrink@outlook.com>
2016-03-03 09:32:07 +0100

    updated .gitignore to ignore all wwwrootJsFiles
    deleted all nwjs files. Want to use only electron

--- src/AspNetCoreAngular2/gulpfile.js
+++ src/AspNetCoreAngular2/gulpfile.js
@@ -16,18 +16,10 @@ var buildConfig = require('./gulp.config');
 gulp.task('electron:windows', function (done) {
     runSeq(
         'clean-temp-electron',
+        'copy-vendor-js-to-wwwroot',
         'copy-electron-files',
         'copy-fonts-to-electron-temp',
         'build-electron',
-        done);
-});
-
-gulp.task('nwjs:windows', function (done) {
-    runSeq(
-        'clean-temp-nwjs',
-        'copy-nwjs-files',
-        'copy-fonts-to-nwjs-temp',
-        'nwjs-build-node-webkit',
         done);
 });
 
@@ -41,46 +33,6 @@ gulp.task('copy:vendor:js:to:wwwroot', function (done) {
 gulp.task('copy-vendor-js-to-wwwroot', function () {
     return gulp.src(buildConfig.config.vendorJsFiles)
         .pipe(gulp.dest(buildConfig.config.rootJsFolder))
-});
-
-gulp.task('copy-nwjs-files', function (done) {
-    return gulp.src([
-        buildConfig.config.assets.nwjs,
-        buildConfig.config.app.allJsFiles,
-        buildConfig.config.app.allCssFiles,
-        buildConfig.config.app.allHtmlFiles,
-        buildConfig.config.app.indexHtmlFile
-    ]).pipe(gulp.dest(buildConfig.config.temp.nwjs));
-});
-
-gulp.task('copy-fonts-to-nwjs-temp', function (done) {
-    return gulp.src(
-        buildConfig.config.allRootFontsFiles
-        ).pipe(gulp.dest(buildConfig.config.temp.nwjsFonts));
-});
-
-gulp.task('nwjs-build-node-webkit', function () {
-
-    var sourcefiles = buildConfig.config.temp.allNwjsFiles;
-    var targetpath = buildConfig.config.dist.nwjsFolder;
-
-    var nw = new NwBuilder({
-        version: '0.12.3',
-        files: sourcefiles, // use the glob format
-        buildDir: targetpath,
-        platforms: ['win32']
-        //platforms: ['osx32', 'osx64', 'win32', 'win64', 'linux32','linux64']
-    });
-
-    nw.on('log', console.log);
- 
-    // Build returns a promise
-    nw.build().then(function () {
-        console.log('all done!');
-    }).catch(function (error) {
-        console.error(error);
-    });
-
 });
 
 gulp.task('copy-fonts-to-electron-temp', function (done) {
@@ -117,10 +69,6 @@ gulp.task('clean-vendor-js-in-root', function (done) {
 
 gulp.task('clean-temp-electron', function (done) {
     return del(buildConfig.config.temp.electron, done);
-});
-
-gulp.task('clean-temp-nwjs', function (done) {
-    return del(buildConfig.config.temp.nwjs, done);
 });
 
 gulp.task('clean-app-js-and-jsmap', function (done) {
