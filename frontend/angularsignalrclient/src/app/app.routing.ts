077443ab0a9e9602a4da13f3b19934ac2d86eab3
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2019-11-01 17:51:44 +0100

    migrate workspace for @angular/cli@9.0.0-next.6

--- frontend/angularsignalrclient/src/app/app.routing.ts
+++ frontend/angularsignalrclient/src/app/app.routing.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', loadChildren: './home/home.module#HomeModule' },
-  { path: 'about', loadChildren: './about/about.module#AboutModule' }
+  { path: 'dashboard', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }
 ];
